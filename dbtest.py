1945dcf619829547090c43a7e9deea04e2f40928
Dan <d-joaca@online.no>
2025-04-15 15:22:49 +0200

    work on dbtest script, timing and query counting

--- dbtest.py
+++ dbtest.py
@@ -6,7 +6,7 @@ import sys
 
 def connectToDatabase():
     databaseConnection = mysql.connector.connect(
-            host='192.168.0.19',
+            host='192.168.0.10',
             user='root',
             password='voxicon',
             database='Test'
@@ -18,11 +18,18 @@ def threadPrint(threadID, texttoprint):
 
 def runQuery(tID, startTime, endTime):
     global queries_ran
+    #global debugInfo
+    debugInfo.append(["Thread:", tID])
     loops = 0
     queries_ran[tID] = 0
     while True:
+        #debugInfo[tID].append("Before DBconn"+str(time.time()))
+        
         dibi = connectToDatabase()
         dbcursor = dibi.cursor()
+
+        #debugInfo[tID].append("After DBconn"+str(time.time()))
+
         #for i in range(1, 5):
            # seed = randint(1, 300)
            # start = seed*10
@@ -34,24 +41,28 @@ def runQuery(tID, startTime, endTime):
            # threadPrint(tID, len(dbcursor.fetchall()))
 
 #        query = "call StressTest('1000', '10')"
-        seed = randint(1, 300)
-        start = seed*10
-        query = f"select * from tpcc.customer where c_id between {start} and {start+20};"
-        threadPrint(tID, "Current query:"+query)
+        seed = randint(1, 30000)
+        start = seed*1
+        #query = f"select * from tpcc.customer where c_id between {start} and {start+10};"
+        query = f"select * from tpcc.customer where c_id like {start};"
+
         dbcursor.execute(query)
 
         #threadPrint(tID, len(dbcursor.fetchall()))
         loops = loops+1
+        dibi.close()
         curTime = time.time()
         int_startTime = int(startTime)
         int_curTime = int(curTime)
         int_endTime = int(endTime)
         threadPrint(tID, f"Time Elapsed: {int_curTime-int_startTime}s / {int_endTime-int_startTime}s")
+
         if curTime > endTime:
             threadPrint(tID, "Exited. Iterations:"+str(loops))
             break
         else:
             queries_ran[tID] = queries_ran[tID] + 1
+            threadPrint(tID, "query counted. Current amount:"+str(queries_ran[tID]))
     
 try:
     if sys.argv[1]: 
@@ -65,16 +76,20 @@ try:
         runTime = int(sys.argv[2])
 except:
     runTime = 5
-queries_ran = [i for i in range(0, threadcount)]
+queries_ran = [i for i in range(0, threadcount-1)]
 startTime = time.time()
 runTime = runTime * 60
 endTime = startTime+runTime
+
+debugInfo = []
+
 allthreads = []
 for num in range(0, threadcount-1):
     allthreads.append(threading.Thread(target=runQuery, args=(num,startTime, endTime)))
 for num in range(0, threadcount-1):
-    time.sleep(0.5)
+    time.sleep(0.05)
     allthreads[num].start()
+    print("Started thread: ", num)
 
 # wait for all threads to finish
 for num in range(0, threadcount-1):
@@ -83,7 +98,11 @@ for num in range(0, threadcount-1):
 total = 0
 for n in queries_ran:
     total = total + n
-print("Total queries finished:", total)
+#print(debugInfo)
+print("\n\n")
+print(queries_ran)
+print(f"Total queries finished in {runTime/60} minutes:", total)
+
 #threadsStarted = 0
 #while True:
 #    if startTime > (time.time() + 60):
